0c2344acfca5a9e0d33c9ca8d3b59c76b84ed5b1
Johnny <johnnychan0302@gmail.com>
2015-07-15 13:06:45 +0100

    update resumeBuilder for testing

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+$("#main").append("Johnny Chan");
+
+var firstName = "Johnny";
+var age = 29;
+console.log(firstName);
+
+var awesomeThought = "I am Johnny and I am awesome";
+console.log(awesomeThought);
